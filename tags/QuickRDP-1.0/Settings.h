2d3db9477d8fa61a79030126ffa83d0243073523
arnestig <arnestig@604a4905-b7d6-43c3-937c-95a39435b2e0>
2011-12-16 18:47:56 +0000

    Tagging release 1.0

---
+++ tags/QuickRDP-1.0/Settings.h
@@ -0,0 +1,52 @@
+/**
+    Copyright (C) 2010 quickRDP - Remote desktop organizer
+
+    Written by Tobias Eliasson <arnestig@users.sourceforge.net>.
+
+    This file is part of quickRDP <http://sourceforge.net/projects/quickrdp/>.
+
+    quickRDP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    quickRDP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with quickRDP.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#ifndef __SETTINGS_H__
+#define __SETTINGS_H__
+
+#include <wx/string.h>
+
+class Settings
+{
+    public:
+        Settings();
+        ~Settings();
+
+        void saveSettings();
+        wxString getRDPExec( bool useAdminString = false ) const;
+        wxString getPuttyExec() const;
+        wxString getPlinkExec() const;
+        void setPlinkExec( wxString plinkExec );
+        void setPuttyExec( wxString puttyExec );
+        wxString getSettingsPath() const;
+        wxString getDatabasePath() const;
+
+    private:
+        void loadSettings();
+
+        wxString RDPExec;
+        wxString puttyExec;
+        wxString plinkExec;
+        wxString settingsPath;
+        wxString databasePath;
+};
+
+#endif
