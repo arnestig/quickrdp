bf438c9e6dbd39499eb6f876cfe8ea13ecf87438
arnestig <arnestig@604a4905-b7d6-43c3-937c-95a39435b2e0>
2012-03-19 18:03:10 +0000

    - Updated version for 1.1.6

--- trunk/src/version.h
+++ trunk/src/version.h
@@ -28,7 +28,7 @@
 
 namespace Version {
 	static wxString name = wxT("QuickRDP");
-	static wxString version = wxT("1.1.5");
+	static wxString version = wxT("1.1.6");
 
 	inline wxString getShortVersion()
 	{
