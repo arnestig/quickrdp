92cd490a7372bd1382d6c62fa638e542b9e3c725
arnestig <arnestig@604a4905-b7d6-43c3-937c-95a39435b2e0>
2012-03-25 19:18:13 +0000

    - Updating version number for 1.2 branch

--- branches/1_2_release/src/version.h
+++ branches/1_2_release/src/version.h
@@ -28,7 +28,7 @@
 
 namespace Version {
 	static wxString name = wxT("QuickRDP");
-	static wxString version = wxT("1.1.6");
+	static wxString version = wxT("1.2");
 
 	inline wxString getShortVersion()
 	{
